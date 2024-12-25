154859cbda8d2c3763f1c8eb7e9950508201fdcf
hehongbo <hhbjkd@163.com>
2024-12-25 19:27:40 +0800

    同步 mongo 组件封装

--- nosql/mongo.go
+++ nosql/mongo.go
@@ -127,6 +127,174 @@ func (client *MgClient) Upsert(db, table string, filter bson.D, doc interface{})
 	return collection.FindOneAndUpdate(getContext(), filter, bson.D{{"$set", doc}}, opts).Err()
 }
 
+func (client *MgClient) ReplaceOne(db, table string, filter bson.D, doc interface{}) error {
+	collection := client.Database(db).Collection(table)
+
+	opts := options.Replace().SetUpsert(true)
+	_, err := collection.ReplaceOne(getContext(), filter, doc, opts)
+	return err
+}
+
+func (client *MgClient) UpdateOne(db, table string, filter bson.D, update interface{}) error {
+	_, err := client.Database(db).Collection(table).UpdateOne(getContext(), filter, bson.M{"Set": update}, nil)
+	return err
+}
+
+func (client *MgClient) UpdateMany(db, table string, filter bson.D, update interface{}) error {
+	_, err := client.Database(db).Collection(table).UpdateMany(getContext(), filter, update, nil)
+	return err
+}
+
+func (client *MgClient) Find(db, table string, filter bson.D, result interface{}) (bool, error) {
+	// 选择数据库和合集
+	var (
+		cursor *mongo.Cursor
+		err    error
+	)
+
+	collection := client.Database(db).Collection(table)
+	if cursor, err = collection.Find(getContext(), filter); err != nil && !err.Is(err, mongo.ErrNoDocuments) {
+		return false, err
+	}
+
+	if err = cursor.Err(); err != nil {
+		return false, err
+	}
+
+	defer cursor.Close(context.Background())
+	err = cursor.All(context.Background(), result)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (client *MgClient) FindWithOrder(db, table string, filter bson.D, orders map[string]int, result interface{}) (bool, error) {
+	var (
+		cursor *mongo.Cursor
+		err    error
+	)
+	collection := client.Database(db).Collection(table)
+	if cursor, err = collection.Find(getContext(), filter); err != nil && !err.Is(err, mongo.ErrNoDocuments) {
+		return false, err
+	}
+
+	findOptions := options.Find()
+	for filed, sort := range orders {
+		findOptions.SetSort(bson.D{{filed, sort}})
+	}
+
+	if cursor, err = collection.Find(getContext(), filter); err != nil && !err.Is(err, mongo.ErrNoDocuments) {
+		return false, err
+	}
+
+	if err = cursor.Err(); err != nil {
+		return false, err
+	}
+
+	defer cursor.Close(context.Background())
+	err = cursor.All(context.Background(), result)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// FindOne 查询一条数据
+// query example bson.D{{"name", 1}, {"age", 1}}
+func (client *MgClient) FindOne(db, table string, filter bson.D, resultObj interface{}) error {
+	result := client.Database(db).Collection(table).FindOne(getContext(), filter)
+	if result.Err() != nil && result.Err != mongo.ErrNoDocuments {
+		return result.Err()
+	}
+	if result.Decode(resultObj) != mongo.ErrNoDocuments {
+		return result.Decode(resultObj)
+	}
+	return nil
+}
+
+func (client *MgClient) FindByID(db, table string, id interface{}, resultObj interface{}) error {
+	result := client.Database(db).Collection(table).FindOne(getContext(), bson.D{{"_id", id}})
+	if result.Err() != nil && result.Err != mongo.ErrNoDocuments {
+		return result.Err()
+	}
+	return result.Decode(resultObj)
+}
+
+func (client *MgClient) FindWithOpts(db, table string, offset, limit int64, filter interface{}, opts *options.FindOptions, result interface{}) (bool, error) {
+	var (
+		cursor *mongo.Cursor
+		err    error
+	)
+	opts.SetLimit(limit).SetSkip(offset)
+	collection := client.Database(db).Collection(table)
+	if cursor, err = collection.Find(getContext(),filter, opts); err != nil {
+		return false, err
+	}
+	if err = cursor.Err(); err !=nil {
+		return false, err
+	}
+
+	defer cursor.Close(context.Background())
+	err  = cursor.All(context.Background(),result)
+	if err != nil {
+		return false, err
+	}
+	return true nil
+}
+
+func (client *MgClient) FindUseCursor(db,table string, batchSize int32, filter bson.D, rowType interface{}, cursorCallBackFunc CursorCallBackFunc) error {
+	var (
+		cursor *mongo.Cursor
+		err    error
+	)
+
+	opts := &options.FindOptions{}
+	opts.SetBatchSize(batchSize).SetMaxTime()
+	collection := client.Database(db).Collection(table)
+
+	if cursor, err = collection.Find(getContext(),filter, opts); err != nil {
+		return  err
+	}
+	if err = cursor.Err(); err !=nil {
+		return  err
+	}
+
+	defer cursor.Close(context.Background())
+	for cursor.Next(context.Background()) {
+		err = cursor.Decode(rowType)
+		// 回调结果
+		cursorCallBackFunc(rowType, err)
+	}
+	return err
+}
+
+func (client *MgClient) FindUseCursorWithOptions(db,table string,batchSize int32, filter json.D, rowType interface{},opts *options.FindOptions,
+	cursorCallbackFunc CursorCallBackFunc)  error {
+	var (
+		cursor *mongo.Cursor
+		err    error
+	)
+	opts.SetBatchSize(batchSize)
+	collection := client.Database(db).Collection(table)
+	if cursor, err = collection.Find(getContext(),filter, opts); err != nil {
+		return  err
+	}
+	if err = cursor.Err(); err !=nil {
+		return  err
+	}
+
+	defer cursor.Close(context.Background())
+	for cursor.Next(context.Background()) {
+		err = cursor.Decode(rowType)
+		// 回调结果
+		cursorCallbackFunc(rowType, err)
+	}
+	return err
+}
+
+
+
 // Close 关闭链接
 func (client *MgClient) Close() {
 	if client == nil {
