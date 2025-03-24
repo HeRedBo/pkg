0c51a99b02c08ccc3fe11f9788d20da1bd68fcba
hehongbo <hhbjkd@163.com>
2025-03-24 19:00:42 +0800

    logger 日志 测试用例调整

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,14 +1,17 @@
 package logger
 
 import (
+	"errors"
+	"go.uber.org/zap"
 	"testing"
 )
 
 func TestLogger(t *testing.T) {
-	//logger := GetLogger()
-	//defer logger.Sync()
-	//err = errors.New("pkg error")
-	//logger.Error("err occurs", WrapMeta(nil, NewMeta("para1", "value1"), NewMeta("para2", "value2"))...)
+	logger := InitLogger()
+	defer logger.Sync()
+	err := errors.New("pkg error")
+	logger.Error("err occurs", zap.Error(err))
+	logger.Error("err occurs", WrapMeta(nil, NewMeta("para1", "value1"), NewMeta("para2", "value2"))...)
 	Info("this a info msg")
 
 }
