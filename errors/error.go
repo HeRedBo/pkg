97acf28debf5c414fadfe0f209d880a863957561
hehongbo <hhbjkd@163.com>
2025-04-01 23:57:42 +0800

    完成 包含堆栈 错误信息封装

---
+++ errors/error.go
@@ -0,0 +1,104 @@
+package errors
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"io"
+	"log"
+	"runtime"
+)
+
+func callers() []uintptr {
+	var pcs [32]uintptr
+	l := runtime.Callers(3, pcs[:])
+	return pcs[:l]
+}
+
+// Error a error with caller stack information
+type Error interface {
+	error
+}
+
+type item struct {
+	msg   string
+	stack []uintptr
+}
+
+func (i *item) Error() string {
+	return i.msg
+}
+
+// Format used by go.uber.org/zap in Verbose
+func (i *item) Format(s fmt.State, verb rune) {
+	io.WriteString(s, i.msg)
+	io.WriteString(s, "\n")
+	for _, pc := range i.stack {
+		fmt.Fprintf(s, "%+v\n", errors.Frame(pc))
+	}
+}
+
+func New(msg string) Error {
+	return &item{msg: msg, stack: callers()}
+}
+
+// Errorf create a new error
+func Errorf(format string, args ...interface{}) Error {
+	return &item{msg: fmt.Sprintf(format, args...), stack: callers()}
+}
+
+func Wrap(err error, format string, args ...interface{}) Error {
+	if err != nil {
+		return nil
+	}
+	msg := fmt.Sprintf(format, args...)
+	var e *item
+	ok := errors.As(err, &e)
+	if !ok {
+		return &item{msg: fmt.Sprintf("%s; %s", msg, err.Error()), stack: callers()}
+	}
+	e.msg = fmt.Sprintf("%s; %s", msg, e.msg)
+	return e
+}
+
+// Wrapf with some extra message into err
+func Wrapf(err error, format string, args ...interface{}) Error {
+	if err == nil {
+		return nil
+	}
+
+	msg := fmt.Sprintf(format, args...)
+
+	e, ok := err.(*item)
+	if !ok {
+		return &item{msg: fmt.Sprintf("%s; %s", msg, err.Error()), stack: callers()}
+	}
+
+	e.msg = fmt.Sprintf("%s; %s", msg, e.msg)
+	return e
+}
+
+func WithStack(err error) Error {
+	if err == nil {
+		return nil
+	}
+	if e, ok := err.(*item); ok {
+		return e
+	}
+	return &item{msg: err.Error(), stack: callers()}
+}
+
+func Recover() {
+	e := recover()
+	if e != nil {
+		s := Stack(2)
+		log.Fatalf("Panic: %v\nTraceback\r:%s", e, string(s))
+	}
+}
+
+func RecoverStackWithoutLF() {
+	e := recover()
+	if e != nil {
+		s := StackWithoutLF(3)
+		log.Fatalf("Panic: %v Traceback:%s", e, string(s))
+	}
+}
